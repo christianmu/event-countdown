d53c6b29d0308e25cde9d9c559b1da9b49ffd72e
Christian <musiol@post.cz>
2023-05-07 20:35:45 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const eventTimeDisplay = document.getElementById("event-time-display");
+const rocket = document.getElementById("rocket");
+
+const liftoffTime = new Date("July 1, 1999, 12:00:00");
+
+const currentTime = new Date();
+console.log(liftoffTime);
+console.log(currentTime);
---
+++ todos.md
@@ -0,0 +1,4 @@
+⏳ Setting the event time
+⏳ Creating and displaying a countdown
+⏳ Animating the countdown
+⌛️ Indicating that the countdown has ended
